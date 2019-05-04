ec7b473cd985c513512781dd2a06a89ca8d980e5
aonomike <aonomike@gmail.com>
2019-05-04 11:21:14 +0300

    resolve ganache provider issue

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 const { interface, bytecode } = require('../compile')
 
 let accounts
@@ -12,6 +13,8 @@ beforeEach( async ()=>{
     inbox = await new web3.eth.Contract(JSON.parse(interface))
             .deploy({ data: bytecode, arguments: ['Hi there!'] })
             .send( { from: accounts[0], gas: '1000000'} )
+
+    inbox.setProvider(provider)
 })
 describe('sample', () => {
     it('has accounts', () => {
